52c35422f5bbd6021c4bd72025258492f060e9e1
Woophy101 <37969343+Woophy101@users.noreply.github.com>
2018-06-03 04:04:20 -0400

    Menu fixes

--- Assets/Scripts/HUD & Menus/MainMenu.cs
+++ Assets/Scripts/HUD & Menus/MainMenu.cs
@@ -6,11 +6,59 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void PlayGame(){
-		SceneManager.LoadScene ("MG");
+
+	public static bool GameIsPaused = false;
+	public GameObject PauseMenuUI;
+	public AudioSource audiosource;
+	float volume;
+
+	void Start(){
+
+		if (PauseMenuUI != null) {
+			PauseMenuUI.SetActive (false);
+		}
+		if  (audiosource != null){
+			volume = audiosource.volume;
+		}
+	}
+
+	void Update(){
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (GameIsPaused) {
+				Resume ();
+			} else {
+				Pause ();
+			}
+		}
+
+
+	}
+		
+	public void ToScene(string scene){
+		SceneManager.LoadScene (scene);
 	}
 
 	public void QuitGame (){
 		Application.Quit ();
+	}
+
+	public void ResumeGame(){
+		
+	}
+
+
+	void Resume(){
+		PauseMenuUI.SetActive (false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+		audiosource.volume = volume;
+	}
+
+	void Pause (){
+		PauseMenuUI.SetActive (true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+		audiosource.volume = volume * 0.5f;
 	}
 }
